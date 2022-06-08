149c72320574cefc9fa855a736bcf35dab848ec3
shadowhung <shadowhung7@gmail.com>
2022-06-08 17:04:44 +0800

    更新语言包

--- application/index/lang/zh-cn/wanlshop/icon.php
+++ application/index/lang/zh-cn/wanlshop/icon.php
@@ -2,14 +2,14 @@
 
 return [
     'Id' => 'ID',
-    'Name' => '名稱',
-    'Icon' => '圖標',
-    'Class' => '類名',
-    'Createtime' => '記入時間',
-    'Updatetime' => '更新時間',
-    'Deletetime' => '删除時間',
-    'Weigh' => '權重',
-    'Status' => '狀態'
+    'Name' => 'Name',
+    'Icon' => 'Icon',
+    'Class' => 'Class',
+    'Createtime' => 'Create time',
+    'Updatetime' => 'Update time',
+    'Deletetime' => 'Delete time',
+    'Weigh' => 'Weigh',
+    'Status' => 'Status'
 ];
 /*return [
     'Id'         => 'ID',
